6e3902adea65b6125d6b519e69151fa03d88ac59
persianpros <persianpros@yahoo.com>
2020-05-25 05:10:16 +0430

    Import the first cleaned version for Open Vision

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import setup_translate
+
+pkg = 'Extensions.NGsetting'
+setup (name = 'enigma2-plugin-extensions-ngsetting',
+       version = '1.1',
+       description = 'NGsetting plugin for Vhannibal settings',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'},
+       package_data = {pkg: ['Po/*/LC_MESSAGES/*.mo', 'Skin/Main.xml', 'Panel/*.png', 'Moduli/NGsetting/rules.xml']},
+       cmdclass = setup_translate.cmdclass, # for translation
+      )
